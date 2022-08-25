fd1223b5311f9e419648a08db6708dd027189eb2
karen <kvlo1998@gmail.com>
2022-08-24 19:31:29 -0500

    Ajuste calculadora

---
+++ main.js
@@ -0,0 +1,35 @@
+let numeroUno = 0;
+let numeroDos = 0;
+let resultado = 0;
+let operacion = "";
+
+function igualar() {
+    if (operacion == '+') {
+        resultado = numeroUno + numeroDos
+    }
+    if (operacion == '-') {
+        resultado = numeroUno - numeroDos
+    }
+    if (operacion == '*') {
+        resultado = numeroUno * numeroDos
+    }
+    if (operacion == '/') {
+        resultado = numeroUno / numeroDos
+    }
+    document.getElementById("display").value = resultado;
+}
+
+function ingresarNumero(x) {
+    if (operacion == "") {
+        numeroUno = x;
+        document.getElementById("display").value = numeroUno;
+    } else {
+        numeroDos = x;
+        document.getElementById("display").value = numeroDos;
+    }
+}
+
+function agregarOperador(ope){
+    operacion = ope;
+}
+
